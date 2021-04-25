e8ebd87c8809d7d8757165e21b479c696042a042
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2021-04-25 23:16:10 +0700

    First build
    And a few more optimization to wrapper generation

--- Assets/Scripts/ClickPop.cs
+++ Assets/Scripts/ClickPop.cs
@@ -8,17 +8,6 @@ public class ClickPop : MonoBehaviour
     public int PopIndexY;
 
     public bool IsPop;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnMouseDown()
     {
--- Assets/Scripts/WrapperManager.cs
+++ Assets/Scripts/WrapperManager.cs
@@ -19,16 +19,28 @@ public class WrapperManager : MonoBehaviour
         }
     }
 
+    public class BubbleGenerationProfile
+    {
+        public int RowXAmount = 10;
+        public int RowYAmount = 10;
+        public Vector2 BubbleSize = Vector2.one;
+        public Vector2 BubbleMargin = new Vector2(0.25f, 0.25f);
+        public Vector2 BubbleBorder = new Vector2(0.25f, 0.25f);
+        public bool Zigzag = true;
+    }
+
+    [SerializeField]
+    public BubbleGenerationProfile CurrentProfile = new BubbleGenerationProfile();
+
+    public bool[,] PopMarker;
+
     public AudioClip[] PopSounds;
     public AudioSource PopPlayer;
-    public bool[,] PopMarker;
     public GameObject BubblePrototype;
-    public int RowXAmount = 10;
-    public int RowYAmount = 10;
-    public Vector2 RowMargin;
-    public Vector2 BubbleSize;
     public List<GameObject> ChildBubbles = new List<GameObject>();
     public GameObject BubbleSheet;
+    public Transform BubbleSheetBackground;
+    public Transform BubbleMask;
 
     // Start is called before the first frame update
     void Start()
@@ -37,16 +49,14 @@ public class WrapperManager : MonoBehaviour
         {
             PopPlayer = GameObject.Find(nameof(PopPlayer)).GetComponent<AudioSource>();
         }
+        GenerateBubbleSheet();
+        ResizeSheetBackground();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GenerateBubbleSheet();
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             foreach (var obj in ChildBubbles)
             {
@@ -67,12 +77,10 @@ public class WrapperManager : MonoBehaviour
 
     public void GenerateBubbleSheet()
     {
-        PopMarker = new bool[10, 10];
-        int rowX = PopMarker.GetUpperBound(0);
-        int rowY = PopMarker.GetUpperBound(1);
-        for (int x = 0;x < rowX; x++)
+        PopMarker = new bool[CurrentProfile.RowXAmount, CurrentProfile.RowYAmount];
+        for (int x = 0;x < CurrentProfile.RowXAmount; x++)
         {
-            for (int y = 0;y < rowY; y++)
+            for (int y = 0;y < CurrentProfile.RowYAmount; y++)
             {
                 //Generate bubble for the row
                 var bubble = Instantiate(BubblePrototype, transform);
@@ -92,17 +100,41 @@ public class WrapperManager : MonoBehaviour
 
     public Vector3 GetPositionForBubble(float x, float y)
     {
-        float axisX = 0;
-        float axisY = 0;
-        float halfX = RowXAmount / 2.0f;
-        float halfY = RowYAmount / 2.0f;
-        axisX = ((BubbleSize.x + RowMargin.x) * x) - ((BubbleSize.x + RowMargin.x) * halfX);
-        axisY = ((BubbleSize.y + RowMargin.y) * y) - ((BubbleSize.y + RowMargin.y) * halfY);
-        //if (x > 0)
-        //    axisX = (BubbleSize.x + RowMargin.x) * x;
-        //if (y > 0)
-        //    axisY = (BubbleSize.y + RowMargin.y) * y;
+        //Move X and Y half from where it was
+        x -= CurrentProfile.RowXAmount / 2;
+        y -= CurrentProfile.RowYAmount / 2;
+        //Add half a size of bubble size as a padding for even amount of bubble
+        if (CurrentProfile.RowXAmount % 2 == 0)
+            x += CurrentProfile.BubbleSize.x / 4;
+        if (CurrentProfile.RowYAmount % 2 == 0)
+            y += CurrentProfile.BubbleSize.y / 4;
+        float axisX = ((CurrentProfile.BubbleSize.x + CurrentProfile.BubbleMargin.x) * x) + CurrentProfile.BubbleBorder.x;
+        float axisY = ((CurrentProfile.BubbleSize.y + CurrentProfile.BubbleMargin.y) * y) + CurrentProfile.BubbleBorder.y;
+        //Subtract half wrapper size
+        //float axisX = 
+        //float axisX = ((CurrentProfile.BubbleSize.x + CurrentProfile.BubbleMargin.x) * x) - 
+        //    (CurrentProfile.BubbleSize.x + CurrentProfile.BubbleMargin.x + 
+        //    (CurrentProfile.RowXAmount % 2 == 0 ? CurrentProfile.BubbleSize.x / 2 : 0));
+        //float axisY = 0;
+        //axisY = ((CurrentProfile.BubbleSize.y + CurrentProfile.BubbleMargin.y) * y) - ((CurrentProfile.BubbleSize.y + CurrentProfile.BubbleMargin.y) * halfY);
+        
         return new Vector3(axisX, axisY);
 
     }
+
+    private void ResizeSheetBackground()
+    {
+        //X Scale
+        float XScale = (CurrentProfile.RowXAmount * CurrentProfile.BubbleSize.x) + 
+            (CurrentProfile.RowXAmount * CurrentProfile.BubbleMargin.x) + 
+            CurrentProfile.BubbleBorder.x;
+        //Y Scale
+        float YScale = (CurrentProfile.RowYAmount * CurrentProfile.BubbleSize.y) +
+            (CurrentProfile.RowYAmount * CurrentProfile.BubbleMargin.y) +
+            CurrentProfile.BubbleBorder.y;
+        //Set
+        BubbleSheetBackground.transform.localScale = new Vector3(XScale, YScale);
+        BubbleMask.transform.localScale = new Vector3(XScale, YScale);
+    }
+
 }
