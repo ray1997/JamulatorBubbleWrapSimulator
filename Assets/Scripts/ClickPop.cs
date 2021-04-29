61738582434f399e6cedf5e0dadb81dddf2493d9
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2021-04-29 14:20:53 +0700

    Popped bubble indicator

--- Assets/Scripts/ClickPop.cs
+++ Assets/Scripts/ClickPop.cs
@@ -22,7 +22,7 @@ public class ClickPop : MonoBehaviour
         //Play pop sound
         WrapperManager.Instance.PlayPop(this);
         //Mark as popped
-
+        TurnPopped();
     }
 
     public void ValidatePop()
@@ -31,17 +31,24 @@ public class ClickPop : MonoBehaviour
         {
             //Turn pop without sound
             //Mark as popped
-            //As a test, for now mark color red onto sprite
+            TurnPopped();
         }
     }
 
-    public void RandomizeWrinkle()
+    /// <summary>
+    /// Select other wrinkle randomly
+    /// </summary>
+    /// <param name="maxAlpha">Give maximum alpha (255/1) instead of randomize it.</param>
+    public void RandomizeWrinkle(bool maxAlpha = false)
     {
         //Select wrinkle
         Wrinkle.sprite = WrapperManager.Instance.BubbleWrinkles[Random.Range(0, WrapperManager.Instance.BubbleWrinkles.Length)];
         //Randomize alpha range
         var color = Wrinkle.color;
-        color.a = Random.Range(0.05f, 0.8f);
+        if (maxAlpha)
+            color.a = 1;
+        else
+            color.a = Random.Range(0.05f, 0.8f);
         Wrinkle.color = color;
         //Randomize flip
         Wrinkle.flipX = Random.value < 0.5f;
@@ -50,5 +57,17 @@ public class ClickPop : MonoBehaviour
         float x = Random.Range(-0.4f, 0.4f);
         float y = Random.Range(-0.4f, 0.4f);
         Wrinkle.transform.localPosition = new Vector3(x, y, 0);
+    }
+
+    //turn sprite to indicate that this bubble is bursted
+    public void TurnPopped()
+    {
+        //Darken alpha channel
+        SpriteRenderer render = GetComponent<SpriteRenderer>();
+        var bcolor = render.color;
+        bcolor.a = 1;
+        render.color = bcolor;
+        //Randomize other wrinkle
+        RandomizeWrinkle(true);
     }
 }
